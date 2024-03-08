d3a5e470840b48ea5586fc32b1e25cfb92ff7096
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-08 12:07:47 +0100

    code Image class behavior, ran some tests on main

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,38 @@
 import entities.Audio;
 import entities.FileFormats;
+import entities.Image;
 import entities.Video;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println();
         Video test1 = new Video("test1", FileFormats.MP4, 60);
-        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
+//        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
         test1.brightnessDown();
         test1.volumeUp();
-        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
+//        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
         test1.brightnessUp();
         test1.brightnessUp();
         test1.brightnessUp();
         test1.volumeDown();
         test1.volumeDown();
-        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
+//        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
 //        test1.play();
-        System.out.println();
+//        System.out.println();
         Audio test2 = new Audio("test2", FileFormats.MP3, 10);
-        System.out.println(test2.volumeLevel());
+//        System.out.println(test2.volumeLevel());
         test2.volumeUp();
         test2.volumeUp();
-        System.out.println(test2.volumeLevel());
+//        System.out.println(test2.volumeLevel());
         test2.volumeDown();
         test2.volumeDown();
         test2.volumeDown();
-        System.out.println(test2.volumeLevel());
-        test2.play();
+//        System.out.println(test2.volumeLevel());
+//        test2.play();
+//        System.out.println();
+        Image test3 = new Image("test3", FileFormats.JPEG);
+        test3.brightnessUp();
+        test3.brightnessUp();
+        test3.show();
     }
 }
--- src/entities/Image.java
+++ src/entities/Image.java
@@ -1,8 +1,40 @@
 package entities;
 
-public class Image extends MultimediaElement {
+import interfaces.Brightness;
+
+public class Image extends MultimediaElement implements Brightness {
+    // ATTRIBUTES
+    private int brightness;
+
     // CONSTRUCTORS
     public Image(String title, FileFormats format) {
         super(title, format);
+        this.brightness = 5;
+    }
+
+    // METHODS
+    public void show() {
+        System.out.println(this.getFileName() + " showing...; " + this.brightnessLevel());
+    }
+
+    @Override
+    public void brightnessUp() {
+        if (brightness < 10) {
+            brightness++;
+        }
+    }
+
+    @Override
+    public void brightnessDown() {
+        if (brightness > 1) {
+            brightness--;
+        }
+    }
+
+    @Override
+    public String brightnessLevel() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < brightness; i++) str.append("*");
+        return "Brightness: (" + str + ")";
     }
 }
