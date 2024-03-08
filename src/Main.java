45c1a2195bdb82160b98347f241e97490874d09b
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-08 15:57:47 +0100

    coded player main behavior and ran some tests

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,39 @@
-import entities.Audio;
-import entities.FileFormats;
-import entities.Image;
-import entities.Video;
+import entities.*;
+import interfaces.PlayVolume;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println();
-        Scanner sc = new Scanner(System.in);
+        final Scanner sc = new Scanner(System.in);
 
-        System.out.println("INSERT 5 MULTIMEDIA ELEMENTS");
-        System.out.println();
-        System.out.println("Insert a video...");
-        Video video1 = createVideo(sc);
-        System.out.println("Insert a video...");
-        Video video2 = createVideo(sc);
-        System.out.println("Insert an audio...");
-        Audio audio1 = createAudio(sc);
-        System.out.println("Insert an audio...");
-        Audio audio2 = createAudio(sc);
-        System.out.println("Insert an image...");
-        Image image = createImage(sc);
+//        System.out.println("INSERT 5 MULTIMEDIA ELEMENTS");
+//        System.out.println();
+//        System.out.println("Insert a video...");
+//        Video video1 = createVideo(sc);
+//        System.out.println("Insert a video...");
+//        Video video2 = createVideo(sc);
+//        System.out.println("Insert an audio...");
+//        Audio audio1 = createAudio(sc);
+//        System.out.println("Insert an audio...");
+//        Audio audio2 = createAudio(sc);
+//        System.out.println("Insert an image...");
+//        Image image = createImage(sc);
+//
+//        MultimediaElement[] media = {video1, video2, audio1, audio2, image};
+//        player(sc, media);
 
-        sc.close();
-
+        Video test1 = new Video("test1", FileFormats.MP4, 3);
+        Video test2 = new Video("test2", FileFormats.MP4, 3);
+        Video test3 = new Video("test3", FileFormats.MP4, 3);
+        Video test4 = new Video("test4", FileFormats.MP4, 3);
+        Video test5 = new Video("test5", FileFormats.MP4, 3);
+        MultimediaElement[] test = {test1, test2, test3, test4, test5};
+        player(sc, test);
 
 //        TESTS
 
@@ -119,4 +126,60 @@ public class Main {
         System.out.println();
         return new Image(title, FileFormats.JPEG);
     }
+
+    public static void player(Scanner sc, MultimediaElement[] arr) {
+        System.out.println("Select a media (type the corresponding number; type 0 to exit:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i + 1 + " - " + arr[i].getFileName());
+        }
+        System.out.println();
+        String value = sc.next();
+        switch (value) {
+            case "0" -> sc.close();
+            case "1" -> {
+                if (arr[0] instanceof PlayVolume) ((PlayVolume) arr[0]).play();
+                System.out.println();
+                player(sc, arr);
+            }
+            case "2" -> {
+                if (arr[1] instanceof PlayVolume) ((PlayVolume) arr[1]).play();
+                System.out.println();
+                player(sc, arr);
+            }
+            case "3" -> {
+                if (arr[2] instanceof PlayVolume) ((PlayVolume) arr[2]).play();
+                System.out.println();
+                player(sc, arr);
+            }
+            case "4" -> {
+                if (arr[3] instanceof PlayVolume) ((PlayVolume) arr[3]).play();
+                System.out.println();
+                player(sc, arr);
+            }
+            case "5" -> {
+                if (arr[4] instanceof PlayVolume) ((PlayVolume) arr[4]).play();
+                System.out.println();
+                player(sc, arr);
+            }
+            default -> {
+                System.out.println("Invalid input. Try again.");
+                System.out.println();
+                player(sc, arr);
+            }
+        }
+    }
+
+//    public static void manageVolume(Scanner sc, MultimediaElement el) {
+//        System.out.println("SET THE VOLUME OF " + el.getFileName());
+//        System.out.println("Default volume: 5 (min: 0 - max: 10)");
+//        System.out.println("Instructions:");
+//        System.out.println("- Type \"up\" and press Enter to set up the volume (+1)");
+//        System.out.println("- Type \"down\" and press Enter to set down the volume (-1)");
+//        System.out.println("- Press Enter to go next");
+//        String value = sc.nextLine();
+//        if (el instanceof PlayVolume) {
+//            if (Objects.equals(value, "up")) ((PlayVolume) el).volumeUp();
+//            else if (Objects.equals(value, "down")) ((PlayVolume) el).volumeDown();
+//        }
+//    }
 }
