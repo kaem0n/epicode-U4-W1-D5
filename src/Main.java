41837912be9447e846aac0c4bbaa3c1e9f5b5f1c
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-03-08 11:51:39 +0100

    code Audio class behavior, ran some tests on main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
+import entities.Audio;
 import entities.FileFormats;
 import entities.Video;
 
 public class Main {
     public static void main(String[] args) {
-        Video test = new Video("test", FileFormats.MP4, 60);
-        System.out.println(test.brightnessLevel() + "; " + test.volumeLevel());
-        test.brightnessDown();
-        test.volumeUp();
-        System.out.println(test.brightnessLevel() + "; " + test.volumeLevel());
-        test.brightnessUp();
-        test.brightnessUp();
-        test.brightnessUp();
-        test.volumeDown();
-        test.volumeDown();
-        System.out.println(test.brightnessLevel() + "; " + test.volumeLevel());
-        test.play();
+        System.out.println();
+        Video test1 = new Video("test1", FileFormats.MP4, 60);
+        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
+        test1.brightnessDown();
+        test1.volumeUp();
+        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
+        test1.brightnessUp();
+        test1.brightnessUp();
+        test1.brightnessUp();
+        test1.volumeDown();
+        test1.volumeDown();
+        System.out.println(test1.brightnessLevel() + "; " + test1.volumeLevel());
+//        test1.play();
+        System.out.println();
+        Audio test2 = new Audio("test2", FileFormats.MP3, 10);
+        System.out.println(test2.volumeLevel());
+        test2.volumeUp();
+        test2.volumeUp();
+        System.out.println(test2.volumeLevel());
+        test2.volumeDown();
+        test2.volumeDown();
+        test2.volumeDown();
+        System.out.println(test2.volumeLevel());
+        test2.play();
     }
 }
--- src/entities/Audio.java
+++ src/entities/Audio.java
@@ -1,8 +1,45 @@
 package entities;
 
-public class Audio extends MultimediaElement {
+import interfaces.PlayVolume;
+
+public class Audio extends MultimediaElement implements PlayVolume {
+    // ATTRIBUTES
+    private int volume;
+    private final int seconds;
+
     // CONSTRUCTORS
-    public Audio(String title, FileFormats format) {
+    public Audio(String title, FileFormats format, int seconds) {
         super(title, format);
+        this.volume = 5;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public void play() {
+        for (int i = seconds; i > 0 ; i--) {
+            System.out.println(this.getFileName() + " is playing. Remaining time: " + i + " seconds; " + this.volumeLevel());
+            if (i == 1) System.out.println(this.getFileName() + " finished reproduction.");
+        }
+    }
+
+    @Override
+    public void volumeUp() {
+        if (volume < 10) {
+            volume++;
+        }
+    }
+
+    @Override
+    public void volumeDown() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+
+    @Override
+    public String volumeLevel() {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < volume; i++) str.append("!");
+        return "Volume: (" + str + ")";
     }
 }
